5c7ab4cfac77fbfcc837e408bf020472c475bc39
Benjamin <ben.d.maryland@gmail.com>
2021-09-28 09:52:24 -0400

    commit tree 336ad1d7b86a8c51e9c82ec31bb2c862af22f577

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Your code goes here
+// Your code goes heredocument.addEventListener("DOMContentLoaded", function() {
+    
+const text = document.querySelector("#text")
+console.log(text)
+
+
+document.addEventListener("DOMContentLoaded", function() {
+        console.log("The DOM has loaded");
+        text.textContent = "This is really cool!"
+      });
+      
+      console.log(
+        "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+      );
